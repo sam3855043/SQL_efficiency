a74dc49fbb1bfaa0cf1057092033936b931b9f49
samuel-chuang <sam3855043@gmail.com>
2024-12-13 17:18:01 +0800

    supply mentory database_space.sql

--- Oracle/database_sapce.sql
+++ Oracle/database_sapce.sql
@@ -19,3 +19,47 @@ SELECT UPPER(F.TABLESPACE_NAME) "tablespace_name",
    and d.TABLESPACE_NAME = ddf.TABLESPACE_NAME
    --and D.tablespace_name not in   (select distinct (tablespace_name) from dba_undo_extents)
  ORDER BY (D.TOT_GROOTTE_MB - F.TOTAL_BYTES)/ddf.max_bytes desc;
+
+ select occupant_name,occupant_desc,round(space_usage_kbytes/1024/1024,2) GB from v$sysaux_occupants order by 3 desc;
+
+
+select owner,segment_name,bytes/1024/1024,segment_type from dba_segments  where tablespace_name='SYSAUX' 
+and bytes>1024*1024*10
+order by 3 desc;
+
+
+truncate table audsys.aud$unified
+
+
+select table_owner,table_name, partition_name, INTERVAL, HIGH_VALUE, HIGH_VALUE_LENGTH,num_rows,blocks*8/1024 MB,last_analyzed
+  from dba_tab_partitions
+ where table_name = 'AUD$UNIFIED';
+
+ BEGIN
+ DBMS_AUDIT_MGMT.ALTER_PARTITION_INTERVAL(
+   interval_number       => 1,
+   interval_frequency    => 'DAY');
+ END;
+/
+
+BEGIN
+  DBMS_AUDIT_MGMT.SET_LAST_ARCHIVE_TIMESTAMP(
+  AUDIT_TRAIL_TYPE     =>  DBMS_AUDIT_MGMT.AUDIT_TRAIL_UNIFIED,
+  LAST_ARCHIVE_TIME    =>  SYSDATE-10 );
+END;
+/
+
+BEGIN
+DBMS_AUDIT_MGMT.CLEAN_AUDIT_TRAIL(
+audit_trail_type => DBMS_AUDIT_MGMT.AUDIT_TRAIL_UNIFIED,
+container => DBMS_AUDIT_MGMT.CONTAINER_CURRENT,
+use_last_arch_timestamp => FALSE);
+END;
+/
+
+BEGIN
+  DBMS_AUDIT_MGMT.CLEAN_AUDIT_TRAIL(
+  AUDIT_TRAIL_TYPE => DBMS_AUDIT_MGMT.AUDIT_TRAIL_AUD_STD,
+  USE_LAST_ARCH_TIMESTAMP => TRUE);
+END;
+/
