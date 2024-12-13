b33e2131c0140e21ceef487dea956a75e3e5aed5
samuel-chuang <sam3855043@gmail.com>
2024-12-13 09:35:22 +0800

    Init

---
+++ 1.sql
@@ -0,0 +1,13 @@
+SELECT 
+    schemaname, 
+    relname, 
+    n_live_tup, 
+    n_dead_tup, 
+    last_vacuum, 
+    last_analyze
+FROM 
+    pg_catalog.pg_stat_all_tables
+WHERE 
+    schemaname = 'dsdata' 
+    AND relname = 'tt654078_xccz_tmp';
+
